e7ba3f0cc96b1d2a3b1962d6611133b160d293a9
Solkush <40328282+Solkush@users.noreply.github.com>
2020-06-30 18:27:22 -0400

    upto the class type

--- scraper.py
+++ scraper.py
@@ -1,16 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
+import time
 
-startDate = input("Start Date: ")
-endDate = input("End Date: ")
- # Convert dates into Epoch
+d= input('Enter a date in YYYY-MM-DD format') + ' 00:00:00'
+p='%Y-%m-%d %H:%M:%S'
+startEpoch = int(time.mktime(time.strptime(d,p)))
+endEpoch = startEpoch + 1209600
+ticker = input("Ticker: ").upper()
 
 
-ticker = input("Ticker: ")
-URL = 'https://finance.yahoo.com/quote/{Ticker}/history?period1={startEpoch}&period2={endEpoch}&interval=1d&filter=history&frequency=1d'
+URL = f'https://finance.yahoo.com/quote/{ticker}/history?period1={startEpoch}&period2={endEpoch}&interval=1d&filter=history&frequency=1d'
 page = requests.get(URL)
+print(URL)
+html_doc = """<table class="W(100%) M(0)" data-test="historical-prices">"""
+soup = BeautifulSoup(html_doc, "html.parser")
+# results = soup.find()
 
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='')
 
-for result in results:
